e18f9848210afb3c5eb89a7e3cf56a595368330b
Ian <ian.aquinoflores@gmail.com>
2025-03-06 21:09:24 -0600

    Agregar

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 from sqlalchemy import create_engine
-import urlib
+import urllib
 
 class Config(object):
     SECRET_KEY='clave nuevsa'
     SESION_COOKIE_SECURE=False
 
-class DevelopmetConfig(Config):
+class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://root:estefi222@127.0.0.1/bdidgs801'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
